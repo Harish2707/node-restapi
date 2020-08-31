cacc80c720e9842faa87a0395751655c3acd236b
Harish2707 <harish.s.2707@gmail.com>
2020-08-31 11:22:35 +0530

    added user login

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+
 
 router.post('/signup', (req, res) =>{
     User.find({ email: req.body.email })
@@ -43,6 +45,49 @@ router.post('/signup', (req, res) =>{
     })
 });
 
+router.post('/login',(req, res) => {
+    User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+        if (user.length < 1){
+            return res.status(401).json({
+                message: "Auth Failed"
+            });
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) =>{
+            if (err){
+                return res.status(401).json({
+                    message: "Auth Failed"
+                });
+            }
+            if (result){
+                const token = jwt.sign(
+                    {
+                        email: user[0].email,
+                        userId: user[0]._id
+                    }, 
+                    "secretKey",
+                    {
+                        expiresIn: "1h"
+                    }
+                );
+                return res.status(200).json({
+                    message: "Auth Success",
+                    token: token
+                });
+            }else{
+                return res.status(401).json({
+                    message: "Auth Failed"
+                });
+            }
+        })
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    });
+})
 
 router.delete('/userId', (req, res) =>{
     User.deleteOne({ _id: req.params.userId })
